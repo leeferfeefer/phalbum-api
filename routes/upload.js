f7d1eb35ad8c22e77cb5173c3a3d515e541943d4
leeferfeefer <danbfjr@gmail.com>
2021-01-06 20:43:25 -0500

    testing contract

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,12 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/upload', function(req, res) {
-  res.send('received request to upload');
+    if (req?.body?.images?.length > 0) {
+        // Save images here - Do I do one image at a time or all at once?
+        res.send("success!")
+    } else {
+        res.status(400).send('Invalid request body!');
+    }
 });
 
 module.exports = router;
